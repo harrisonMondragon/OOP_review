0c07228c1430ae5eaa61cb7f0df5de6ed9561911
Harry Mondragon <mondragon.harrison@gmail.com>
2024-10-06 15:49:23 -0600

    Add animal parent class and dog child class

---
+++ interview_inspiration/src/Dog.java
@@ -0,0 +1,18 @@
+public class Dog extends Animal{
+    
+    private boolean barksAtStrangers;
+
+    public Dog(String name, int age, boolean barksAtStrangers){
+        super(name, age);
+        this.barksAtStrangers = barksAtStrangers;
+    }
+
+    public boolean getBarksAtStrangers(){
+        return barksAtStrangers;
+    }
+
+    @Override
+    public String toString(){
+        return "Name: " + getName() + ", Age: " + getAge() + ", Barks at strangers: " + getBarksAtStrangers();
+    }
+}
